7b8bd1c69398413bf74d7e05096142e79dac2570
Howard Yeh <howard@metacircus.com>
2016-07-26 17:45:28 +0800

    migrate to new course format

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  lessons: {
+    "taking-the-first-step": {
+      repo: "css-profile",
+    },
+
+    "navigation-and-content": {
+      repo: "css-profile",
+    },
+
+    "more-content-and-form": {
+      repo: "css-profile",
+    },
+
+    "content-part3": {
+      repo: "css-profile",
+    },
+
+    "responsive": {
+      repo: "css-profile",
+    },
+
+  }
+}
+
+
